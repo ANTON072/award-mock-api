f3290d3b12b254400d41b3ab3196a86bcc04ea90
Katsushi OUGI <ougi@to-r.net>
2024-07-01 21:04:18 +0900

    chore: Update npm dependencies for @hono/firebase-auth to version 1.4.1

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,8 @@ const config: VerifyFirebaseAuthConfig = {
   projectId: 'award-project-7042e',
 }
 
-// You can specify here the extended VerifyFirebaseAuthEnv type.
-//
-// If you do not specify `keyStore` in the configuration, you need to set
-// the variables `PUBLIC_JWK_CACHE_KEY` and `PUBLIC_JWK_CACHE_KV` in your
-// wrangler.toml. This is because `WorkersKVStoreSingle` is used by default.
-//
-// For more details, please refer to: https://github.com/Code-Hex/firebase-auth-cloudflare-workers
 const app = new Hono<{ Bindings: VerifyFirebaseAuthEnv }>()
 
-// set middleware
 app.use('*', verifyFirebaseAuth(config))
 app.get('/hello', (c) => {
   const idToken = getFirebaseToken(c) // get id-token object.
